7623966b33d609133c8274e7c5e83213aebcb9ff
kts2442 <https://github.com/kts2442>
2023-01-27 21:49:47 +0900

    edit url for catching all - movie title, movie id

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,7 @@ export default function Home() {
   const router = useRouter();
   const [movies, setMovies] = useState();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
 
   useEffect(() => {
@@ -37,15 +29,7 @@ export default function Home() {
         >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               {movie.original_title}
             </Link>
           </h4>
