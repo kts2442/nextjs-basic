452dad2534db65d67fcb904132e6c42bce69608c
kts2442 <https://github.com/kts2442>
2023-01-27 20:25:27 +0900

    nextjs-basic movie-app layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
