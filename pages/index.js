e6cde27046ff6e46e28b75c2166008815959e429
kts2442 <https://github.com/kts2442>
2023-01-27 21:37:09 +0900

    add link to movie image using router.push

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,54 @@
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import Seo from "../components/Seo";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   const [movies, setMovies] = useState();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+
   useEffect(() => {
     (async () => {
       const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
+
   return (
     <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              {movie.original_title}
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -25,6 +57,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
